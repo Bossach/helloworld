6c029ca5e9dd9f2fe3bd944c9faf260a960e1d61
Bossach <boss52@inbox.ru>
2023-08-18 09:03:37 +0300

    framed output helloworld realized

--- helloworld.c
+++ helloworld.c
@@ -1,10 +1,35 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "helloworld.h"
+
+#define BUFFSIZE 64
 
 int main(int argc, char* argv[]){
 	
-	printf("Hello world\n");
-	for(int i = 1; i < argc; i++)
-		printf("Hello %s\n", argv[i]);
+	char *out[argc];
+	
+	char **ptr;
+	ptr = out;
+		
+	char buff[BUFFSIZE];
+	
+	strcpy(buff, "Hello, world!");
+	
+	*ptr = (char*)malloc(strlen(buff) + 1);
+	
+	strcpy(*ptr, buff);
+	
+	for(int a = 1; a < argc; a++){
+		sprintf(buff, "Hello, %s!",argv[a]);
+		
+		ptr++;
+		*ptr = (char*)malloc(strlen(buff) + 1);
+		
+		strcpy(*ptr, buff);
+	}
+
+	frprint(out, argc);
 	
 	return 0;
 }
